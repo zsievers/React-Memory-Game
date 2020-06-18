af118a87270c34f1b05868f224b754a9959605ce
Zach <zjsievers@gmail.com>
2020-06-18 13:47:06 -0400

    started file structure and created functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Cards from "./components/Cards";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Nav from "./components/Nav";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  // setting this.state.cards to the cards json array
+  state = {
+    vols
+  };
+
+  removeVol = id => {
+// filter for vols with an id not equal to the id being removed
+    const cards = this.state.cards.filter(card => card.id !== id);
+    // this.state.cards equal to the new cards array
+    this.setState({ cards });
+  };
+
+  
+    
+
+
+
+
+
+
 }
 
 export default App;
