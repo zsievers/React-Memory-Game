cf354d75abe3d072c9bf849653b17eafb6e7dbab
Zach <zjsievers@gmail.com>
2020-06-19 14:05:41 -0400

    creating () in app.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,88 @@
-import React, { Component } from 'react';
-import Cards from "./components/Cards";
+import React, { Component } from "react";
+import PlayerCard from "./components/PlayerCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import Nav from "./components/Nav";
-
+import players from "./players.json";
+import "./App.css";
 
 class App extends Component {
-  // setting this.state.cards to the cards json array
+  // setting this.state.players to the players json array
   state = {
-    vols
+    players,
+    clicked: [],
+    score: 0,
+    highScore: 0,
+    message: "Click an image to begin!",
   };
 
-  removeVol = id => {
-// filter for vols with an id not equal to the id being removed
-    const cards = this.state.cards.filter(card => card.id !== id);
-    // this.state.cards equal to the new cards array
-    this.setState({ cards });
+  randomizeCards = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
   };
 
-  
-    
+  handleClick = (id) => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
 
+  handleIncrement = () => {
+    const newScore = this.state.score + 1;
+    this.setState({
+      score: newScore,
+      message: "You Guessed Correctly!",
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({
+        topScore: newScore,
+      });
+    } else if (newScore === 12) {
+      this.setState({
+        message: "You Win!",
+      });
+    }
+    this.handleShuffle();
+  };
 
+  handleReset = () => {
+    this.setState({
+      score: 0,
+      topScore: this.state.topScore,
+      message: "Click an Image to begin!",
+      clicked: [],
+    });
+    this.handleShuffle();
+  };
 
+  handleShuffle = () => {
+    let randomizedPlayers = this.randomizeCards(players);
+    this.setState({ players: randomizedPlayers });
+  };
 
+  render() {
+    return (
+      <Wrapper>
+        <Nav 
+        
+        />
+        <Title> </Title>
 
-
+        {this.state.players.map((player) => (
+          <PlayerCard 
+          
+          
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
