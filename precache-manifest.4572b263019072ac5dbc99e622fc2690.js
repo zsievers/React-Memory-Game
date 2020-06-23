a06b50fed57b05e084da30c8723fe912e2f1fc54
Zach <zjsievers@gmail.com>
2020-06-22 21:19:21 -0400

    Updates

---
+++ precache-manifest.4572b263019072ac5dbc99e622fc2690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f019a8af50ad06a849da7ea6f935c6c",
+    "url": "/React-Memory-Game/index.html"
+  },
+  {
+    "revision": "13fd53bb7fd378c11137",
+    "url": "/React-Memory-Game/static/css/main.d834cb7d.chunk.css"
+  },
+  {
+    "revision": "9a63c1c0ef51056c8a1d",
+    "url": "/React-Memory-Game/static/js/2.e8a595de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Memory-Game/static/js/2.e8a595de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13fd53bb7fd378c11137",
+    "url": "/React-Memory-Game/static/js/main.619c9666.chunk.js"
+  },
+  {
+    "revision": "48db8c08f8b6eefc33d8",
+    "url": "/React-Memory-Game/static/js/runtime-main.3171b450.js"
+  }
+]);
